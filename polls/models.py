416230f5d8e3bb9b6bdec48f5fbfbe37b290499a
Naoki Kaneshiro <naoki.kaneshiro@gmail.com>
2013-03-09 14:57:01 +0900

    「はじめての Django アプリ作成、その 4」のcsrfまで完了

--- polls/models.py
+++ polls/models.py
@@ -8,13 +8,15 @@ class Poll(models.Model):
     question = models.CharField (max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    # hogehoge
     def __unicode__(self):
         return self.question
 
-    # hogehoge
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python manage.py runserver 0.0.0.0:3000
+python manage.py runserver 0.0.0.0
